42fdb73ccf79b897857daf4e129fd15e3de1a287
Ioannis Horgos <jhor@acta.com.gr>
2023-10-28 00:39:28 +0300

    good practices fixes 2

--- hacktoberfest/src/app/shared/participants/participants.component.ts
+++ hacktoberfest/src/app/shared/participants/participants.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { DataSharingService } from 'src/app/services/data-sharing.service';
 
 @Component({
